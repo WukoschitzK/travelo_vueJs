bb829bcdd111a5b19816774498b2d0fc16bb8d47
kerstin <kerstin.wukoschitz@gmx.net>
2022-07-16 13:57:51 +0200

    fix font issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,19 +6,25 @@
     <title>Travelo!</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap');
+        /*@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap');*/
+        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap");
+        #app {
+            font-family: Poppins, Helvetica, Arial, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
     </style>
 </head>
 
 <body>
-<div>
+<div id="app" >
     <div class="lg:flex flex-row-reverse justify-between items-center min-h-screen">
         <div class='bg-black bg-opacity-25 h-72 md:h-96 lg:min-h-screen lg:w-4/5 relative mb-9'>
             <img src="../img/header-img.jpg" alt="car" class="w-full h-full object-cover absolute object-left mix-blend-overlay">
         </div>
 
         <div class="pl-8 md:pl-16 lg:pl-28 lg:pr-32">
-            <h1 class="font-bold text-yellow text-4xl md:text-6xl mb-2.5">Bist du auch
+            <h1 class="font-poppins font-bold text-yellow text-4xl md:text-6xl mb-2.5">Bist du auch
                 Travelo?</h1>
             <p class="text-3xl md:text-4xl lg:text-5xl font-light mb-9">Teile dein Erlebnis mit
                 der ganzen Welt.</p>
@@ -28,11 +34,11 @@
 
     </div>
 {{--    <img src="../img/header-img.jpg" alt="car">--}}
-    <div id="app" class="pt-14 md:pt-20 lg:pt-28 px-8 md:px-16 lg:px-28">
+    <div class="pt-14 md:pt-20 lg:pt-28 px-8 md:px-16 lg:px-28">
         <all-posts></all-posts>
         <create-post></create-post>
     </div>
 </div>
 </body>
-{{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
+<script src="{{ asset('js/app.js') }}" defer></script>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,21 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
+        extend: {},
         fontFamily: {
-            'sans': ['"Poppins"', ...defaultTheme.fontFamily.sans],
-            'serif': [...defaultTheme.fontFamily.serif],
-            'mono': [...defaultTheme.fontFamily.mono]        },
-        extend: {
-            colors: {
-                mainText: "#001427",
-                yellow:  "#E6A900",
-                yellowDark: "#D9A106"
-            },
+            sans: ["Poppins", "Arial", "sans-serif"],
+        },
+        colors: {
+            mainText: "#001427",
+            yellow:  "#E6A900",
+            yellowDark: "#D9A106"
+        },
 
-            maxWidth: {
-                '1/2': '50%',
-                '2/6': '33%'
-            }
+        maxWidth: {
+            '1/2': '50%',
+            '2/6': '33%'
         }
+
     },
   plugins: [],
 }
